b2224decbc446c5e1ba8885278e058a77148fbeb
syxdev <1035985510@qq.com>
2022-04-07 15:36:00 +0800

    1

---
+++ src/main/java/com/example/personaldiary/PersonalDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.personaldiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalDiaryApplication.class, args);
+    }
+
+}
